868f50bf5431dda8a2a9c7d21c371c92812d8424
tthescott <sr24unme@gmail.com>
2025-12-16 12:45:01 -0800

    make add pair button, still need to flesh out modal

--- public/script.js
+++ public/script.js
@@ -1,10 +1,20 @@
-const overlay = document.getElementById("overlay");
-const btnLogo = document.getElementById("btn-logo");
+const infoOverlay = document.getElementById("info-overlay");
+const logoBtn = document.getElementById("logo-btn");
+const addBtn = document.getElementById("add-btn");
+const addPairOverlay = document.getElementById("add-pair-overlay");
 
-// display modal when logo is clicked
-btnLogo.onclick = () => overlay.classList.remove("hidden");
+// display info modal when logo is clicked
+logoBtn.onclick = () => infoOverlay.classList.remove("info-hidden");
 
-// close modal when overlay is clicked
-overlay.onclick = (e) => {
-  if (e.target === overlay) overlay.classList.add("hidden");
+// close info modal when infoOverlay is clicked
+infoOverlay.onclick = (e) => {
+  if (e.target === infoOverlay) infoOverlay.classList.add("info-hidden");
+}
+
+// display add pair overlay when button is clicked
+addBtn.onclick = () => addPairOverlay.classList.remove("add-hidden");
+
+// close add modal when addPairOverlay is clicked
+addPairOverlay.onclick = (e) => {
+  if (e.target === addPairOverlay) addPairOverlay.classList.add("add-hidden");
 }
