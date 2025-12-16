a32a8b41d620f25d98f8ec843090e0395d653fc5
tthescott <sr24unme@gmail.com>
2025-12-16 00:48:11 -0800

    make clicking Afterplay display the info modal

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
+const overlay = document.getElementById("overlay");
+const btnLogo = document.getElementById("btn-logo");
 
+// display modal when logo is clicked
+btnLogo.onclick = () => overlay.classList.remove("hidden");
+
+// close modal when overlay is clicked
+overlay.onclick = (e) => {
+  if (e.target === overlay) overlay.classList.add("hidden");
+}
